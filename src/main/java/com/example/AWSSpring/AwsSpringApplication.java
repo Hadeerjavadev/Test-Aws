0bfdb4ca63b701c38eaa98457067ee490ae3c78b
hadeer <hmagdy@citglobal.com>
2025-07-15 02:55:20 +0300

    Initial Spring Boot project

---
+++ src/main/java/com/example/AWSSpring/AwsSpringApplication.java
@@ -0,0 +1,14 @@
+package com.example.AWSSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSpringApplication.class, args);
+		System.out.println("welcome on AWS spring application");
+	}
+
+}
